e5cb03d86a3ce31f9ee55dcab3712ea069dd078b
dieungocbao <dieungocbao@gmail.com>
2021-08-17 16:07:28 +0700

    change to graphql playground

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import { MikroORM } from '@mikro-orm/core'
-import microCofig from './mikro-orm.config'
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
-import { PostResolver } from './resolvers/post.resolver'
-import { UserResolver } from './resolvers/user.resolver'
-import redis from 'redis'
-import session from 'express-session'
-import connectRedis from 'connect-redis'
-import { __prod__ } from './constants'
-import { MyContext } from './types'
+import { MikroORM } from "@mikro-orm/core";
+import microCofig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post.resolver";
+import { UserResolver } from "./resolvers/user.resolver";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
 
 const main = async () => {
-  const orm = await MikroORM.init(microCofig)
-  await orm.getMigrator().up()
+  const orm = await MikroORM.init(microCofig);
+  await orm.getMigrator().up();
 
-  const app = express()
+  const app = express();
 
-  const RedisStore = connectRedis(session)
-  const redisClient = redis.createClient()
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
 
   app.use(
     session({
-      name: 'qid',
+      name: "qid",
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
@@ -31,26 +32,27 @@ const main = async () => {
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 465 * 10, // 10 years
         httpOnly: true,
-        sameSite: 'lax', // csrf
+        sameSite: "lax", // csrf
         secure: __prod__, // https only
       },
       saveUninitialized: false,
-      secret: 'dieungocbao',
+      secret: "dieungocbao",
       resave: false,
     })
-  )
+  );
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
-  })
-  await apolloServer.start()
-  apolloServer.applyMiddleware({ app })
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () => console.log('server started on localhost:4000'))
-}
+  app.listen(4000, () => console.log("server started on localhost:4000"));
+};
 
-main().catch((err) => console.error(err))
+main().catch((err) => console.error(err));

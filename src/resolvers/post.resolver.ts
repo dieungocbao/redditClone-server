91c35da0a3d62f9b3f87fe24b5fefca0cd4541e0
dieungocbao <dieungocbao@gmail.com>
2021-08-29 16:39:16 +0700

    fieldResolver textSnippet

--- src/resolvers/post.resolver.ts
+++ src/resolvers/post.resolver.ts
@@ -1,24 +1,48 @@
-import { Post } from "../entities/post.entity"
-import { Resolver, Query, Arg, Int, Mutation, Ctx, Authorized } from "type-graphql"
-import { PostInput } from "../dto/postInput.dto"
-import { MyContext } from "../types"
+import { Post } from '../entities/post.entity'
+import {
+  Resolver,
+  Query,
+  Arg,
+  Int,
+  Mutation,
+  Ctx,
+  Authorized,
+  FieldResolver,
+  Root,
+} from 'type-graphql'
+import { PostInput } from '../dto/postInput.dto'
+import { MyContext } from '../types'
+import { getConnection } from 'typeorm'
 
-@Resolver()
+@Resolver((of) => Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 150) + '...'
+  }
+
   @Query(() => [Post])
-  posts(): Promise<Post[]> {
-    return Post.find({})
+  posts(
+    @Arg('limit', () => Int) limit: number,
+    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
+  ): Promise<Post[]> {
+    const realLimit = Math.min(limit, 50)
+    const qb = getConnection().getRepository(Post).createQueryBuilder('p')
+    if (cursor) {
+      qb.where('"createdAt" > :cursor', { cursor: new Date(parseInt(cursor)) })
+    }
+    return qb.orderBy('"createdAt"', 'DESC').take(realLimit).getMany()
   }
 
   @Query(() => Post, { nullable: true })
-  post(@Arg("id", () => Int) id: number): Promise<Post | undefined> {
+  post(@Arg('id', () => Int) id: number): Promise<Post | undefined> {
     return Post.findOne({ _id: id })
   }
 
   @Authorized()
   @Mutation(() => Post)
   async createPost(
-    @Arg("input") input: PostInput,
+    @Arg('input') input: PostInput,
     @Ctx() { req }: MyContext
   ): Promise<Post> {
     // 2 sql queries
@@ -27,21 +51,21 @@ export class PostResolver {
 
   @Mutation(() => Post, { nullable: true })
   async updatePost(
-    @Arg("id", () => Int) id: number,
-    @Arg("title") title: string
+    @Arg('id', () => Int) id: number,
+    @Arg('title') title: string
   ): Promise<Post | null> {
     const post = await Post.findOne({ _id: id })
     if (!post) {
       return null
     }
-    if (typeof title !== "undefined") {
+    if (typeof title !== 'undefined') {
       await Post.update({ _id: id }, { title })
     }
     return post
   }
 
   @Mutation(() => Boolean)
-  async deletePost(@Arg("id", () => Int) id: number): Promise<Boolean> {
+  async deletePost(@Arg('id', () => Int) id: number): Promise<Boolean> {
     try {
       await Post.delete({ _id: id })
       return true
